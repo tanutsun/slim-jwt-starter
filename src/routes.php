1999302b97d0bba835267e7c13dd6d73fd5ce4cb
Tanutsun <tanutsun@gmail.com>
2018-01-17 22:55:19 +0700

    implement auth service

--- src/routes.php
+++ src/routes.php
@@ -34,7 +34,11 @@ $app->post("/token",  function ($request, $response, $args) use ($container){
  
 $app->get("/".VESION."/secure",  function ($request, $response, $args) {
  
-    return include ('../'.VERSION."/secure.php");
+    $data = ["status" => 1, 'msg' => "This route is secure!", 'in '.VERSION];
+    
+    return $response->withStatus(200)
+           ->withHeader("Content-Type", "application/json")
+           ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
 });
  
 $app->get("/".VESION."/not-secure",  function ($request, $response, $args) {
